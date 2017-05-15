7e95339b7cf373409a26bac78a4631cc6303b976
Jaime Gonzalez Bonorino <jaimegbonorino@gmail.com>
2017-05-15 14:51:32 -0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+int main(int argc, char * argv[]){
+    int opc;
+    bool running = true, choosing = true;
+    std::cout<<"**************************"<<std::endl;
+    std::cout<<"* Welcome to WorkOMeter! *"<<std::endl;
+    std::cout<<"**************************"<<std::endl<<std::endl;
+
+    while(running){
+        while(choosing){
+            std::cout<<"Choose an option: "<<std::endl;
+            std::cout<<"1. Create a task."<<std::endl;
+            std::cout<<"2. Complete a task."<<std::endl;
+            std::cout<<"3. Delete a task."<<std::endl;
+        }
+    };
+
+    
+    return 0;
+};
